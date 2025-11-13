3bd477ff934f9b4551a405df343e06377f1c2545
Suraj Singh <suraj.job.2002@gmail.com>
2025-11-13 14:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-// Complete the js code
+ // Complete the js code
 function Car(make, model) {
-	this.make=make;
-	this.model=model;
+  this.make = make;
+  this.model = model;
 }
-car.prototype.getMakeModel=function() {
-	return `${this.make} ${this.model}`
-	
-}
-function SportsCar(make, model, topSpeed) {
-	car.call(thus,make,model)
-	this.topSpeed=topSpeed;
-}
-SportsCar.prototype=object.create(car.prototype)
-SportsCar.prototype.constrcutor=SportsCar;
 
-SportsCar.prototype.getTopSpeed=function() {
-	return this.topSpeed;
-	
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+  // Call the Car constructor
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
 }
+
+// Inherit from Car
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Fix the cons reference
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
